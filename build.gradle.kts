3c6ef9f3566ab0b4c6f3b3b3da780fd996f0da70
mauricewoike <maurice.woike@cstx.de>
2024-05-19 14:31:18 +0200

    First basic implementation of vertex ai pro model request

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.cloud:google-cloud-aiplatform:3.35.0")
+    implementation("com.google.cloud:google-cloud-vertexai:1.3.0")
 }
 
 tasks.test {
